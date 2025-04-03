da8ba7633ad3497bf6fc1eca1d5ea582ff9f1b8f
Satendra Kaushik <satendrakaushik2002@gmail.com>
2025-04-03 11:28:29 +0530

    updatecode

--- app/placements/page.tsx
+++ app/placements/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import DashboardLayout from "@/components/dashboard-layout"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -451,4 +453,3 @@ export default function PlacementsPage() {
     </DashboardLayout>
   )
 }
-
